615f50a13eb06714007c78cf92328d16e53bdd00
ragini <ragini.andey99@gmail.com>
2021-06-26 18:43:03 +0530

    Fixed heroku collect static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
